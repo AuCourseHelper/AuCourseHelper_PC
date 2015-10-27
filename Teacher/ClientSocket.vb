d3b8d5555f26a1eafc20a4630b89cf2ee06b6abb
a0tim <a0tim82326@gmail.com>
2015-10-27 20:42:41 +0800

    20151027-2041

---
+++ Teacher/ClientSocket.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Net
+
+Module ClientSocket
+
+    Public Function GetIPaddress() As String
+        Dim myHost As String = System.Net.Dns.GetHostName
+        Dim myIPs As System.Net.IPHostEntry = System.Net.Dns.GetHostEntry(myHost)
+        For Each ipAddress As System.Net.IPAddress In myIPs.AddressList
+            If ipAddress.ToString.ElementAt(3) = "." Then
+                Return ipAddress.ToString()
+            End If
+        Next
+        Return "Get Ip Fail"
+    End Function
+
+    Public Function GetRealIPaddress() As String
+        Dim wc As New WebClient
+        Dim html = wc.DownloadString("http://whereismyip.com/")
+        Dim nStart = html.IndexOf("""verdana"">") + 10
+        Dim nStop = html.IndexOf("</font></b>")
+        Return html.Substring(nStart, nStop - nStart)
+    End Function
+
+End Module
