c9ab00e9dfc52e297a8c1aa72b64f4e7a76c81db
a0tim <a0tim82326@gmail.com>
2015-12-10 18:14:06 +0800

    pdf位置

--- Teacher/Modules/modExport.vb
+++ Teacher/Modules/modExport.vb
@@ -25,23 +25,39 @@ Module modExport
         'doc.Save(sPath)
         'doc.Close()
         'System.Diagnostics.Process.Start(sPath)
-        Dim doc1 As New Document(PageSize.A4.Rotate()) '宣告 Document 文件
+        Dim doc1 As New Document(PageSize.A4.Rotate(), 25, 25, 25, 25) '宣告 Document 文件
         Dim path As String = "D:\000.pdf" '預設檔案存放的路徑
         Dim filename As String = "test" '檔案名稱
-        Dim para1 As String = "AAAAAAAAAA" + vbCrLf + "BBBBBBBBBBBB" + vbCrLf + "CCCCCCCCCC" '文字段落1。要跳行要用 vbCrLf 。
-        Dim para2 As String = "我是中文字～大家好....." + vbCrLf + "我是中文字～大家好....." '文字段落2
-        PdfWriter.GetInstance(doc1, New FileStream(path, FileMode.Create))
+        Dim para1 As String = "104-1 物件導向程式設計"
+        Dim writer = PdfWriter.GetInstance(doc1, New FileStream(path, FileMode.Create))
         doc1.Open() '開啟文件
         Dim jpgx As iTextSharp.text.Image = iTextSharp.text.Image.GetInstance(img, BaseColor.WHITE)
         jpgx.ScalePercent((doc1.PageSize.Height - 100) / jpgx.Height * 100)
+        jpgx.Alignment = Element.ALIGN_CENTER
+
         Dim fontPath As String = Environment.GetFolderPath(Environment.SpecialFolder.System) & "\..\Fonts\kaiu.ttf"
         Dim bfChinese As BaseFont = BaseFont.CreateFont(fontPath, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED)
-        Dim fontChinese As New Font(bfChinese, 16.0F, Font.NORMAL)
-        Dim content As iTextSharp.text.Paragraph = New iTextSharp.text.Paragraph("" + filename + vbCrLf + para1 + vbCr + para2 + vbCrLf, fontChinese) '將文字段落串起來，並設定文字樣式
+        Dim fontChinese As New Font(bfChinese, 24, Font.BOLD, BaseColor.BLUE)
+        Dim content1 As New iTextSharp.text.Paragraph(para1, fontChinese)
 
-        doc1.Add(content) '插入文字段落內容
-        doc1.Add(jpgx) '插入 JPG/GIF 圖片
-        doc1.Close() ' 關閉輸出文件
+        Dim cb As PdfContentByte = writer.DirectContent
+        cb.BeginText()
+        cb.SetFontAndSize(bfChinese, 12)
+        cb.SetTextMatrix(660, 10)
+        cb.ShowText("座位表匯出日期：" + DateTime.Now.ToShortDateString())
+        cb.SetTextMatrix(500, doc1.PageSize.Height - 55)
+        cb.ShowText("開課班級：資工二B")
+        cb.SetTextMatrix(500, doc1.PageSize.Height - 75)
+        cb.ShowText("授課教師：黃信貿")
+        cb.SetTextMatrix(650, doc1.PageSize.Height - 55)
+        cb.ShowText("上課時間：一/06-09")
+        cb.SetTextMatrix(650, doc1.PageSize.Height - 75)
+        cb.ShowText("上課教室：0712")
+        cb.EndText()
+
+        doc1.Add(content1)
+        doc1.Add(jpgx)
+        doc1.Close()
         Process.Start(path)
     End Sub
 
